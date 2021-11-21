4bba010de2d442bc290247e4dede007095a7a405
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-11-21 15:53:50 +0300

    Added control flow for input of user details

--- js/main.js
+++ js/main.js
@@ -127,6 +127,26 @@ $(documwnt).ready(function(){
       $("#project-eight-title").toggle("#project-eight");
     }
   );
-   
 
-})
+  //control flow for user input in the form
+  $("#form-submission").submit(function(event) {
+    var nameOfUser = $("input#user-name").val();
+    var emailOfUser = $("input#user-email").val();
+
+    if (nameOfUser != "" && emailOfUser != "") {
+      alert(
+        "Hey there " +
+          nameOfUser +
+          ". Thank you for contacting us. We will get in touch with you through your email: " +
+          emailOfUser
+      );
+
+      $("form-submission").reset();
+    } else {
+      alert("Invalid input! Please fill in your name and email address.");
+    }
+
+    event.preventDefault();
+  });   
+
+});
