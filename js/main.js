bbdf7f1fbdf126142078cdf2084c1b1473f1b12b
Victormbaabu <victor.mbaabu@strathmore.edu>
2021-11-21 15:10:57 +0300

    Added functions for mouse hoover on images on the portfolio section

--- js/main.js
+++ js/main.js
@@ -16,5 +16,117 @@ $(documwnt).ready(function(){
       });
 
       //development
+      $("#development-icon").click(function() {
+        $("#development-icon").hide();
+        $("#development-icon-description").toggle();
+        $("#development-icon-description").show();
+      });
+      $("#development-icon-description").click(function() {
+        $("#development-icon-description").hide();
+        $("#development-icon").toggle();
+        $("#development-icon").show();
+      });
+
+      //product management
+      $("#product-icon").click(function() {
+        $("#product-icon").hide();
+        $("#product-icon-description").toggle();
+        $("#product-icon-description").show();
+      });
+      $("#product-icon-description").click(function() {
+        $("#product-icon-description").hide();
+        $("#product-icon").toggle();
+        $("#product-icon").show();
+      });
+
+      //function for mouse hover on work headers
+      //project one
+  $("#project-one").hover(
+    function() {
+      $("#project-one").animate({ opacity: "0.35" });
+      $("#project-one-title").toggle("#project-one");
+    },
+    function() {
+      $("#project-one").animate({ opacity: "1" });
+      $("#project-one-title").toggle("#project-one");
+    }
+  );
+  $("#project-two").hover(
+    function() {
+      $("#project-two").animate({ opacity: "0.35" });
+      $("#project-two-title").toggle("#project-two");
+    },
+    function() {
+      $("#project-two").animate({ opacity: "1" });
+      $("#project-two-title").toggle("#project-two");
+    }
+  );
+  //project three
+  $("#project-three").hover(
+    function() {
+      $("#project-three").animate({ opacity: "0.35" });
+      $("#project-three-title").toggle("#project-three");
+    },
+    function() {
+      $("#project-three").animate({ opacity: "1" });
+      $("#project-three-title").toggle("#project-three");
+    }
+  );
+  //project four
+  $("#project-four").hover(
+    function() {
+      $("#project-four").animate({ opacity: "0.35" });
+      $("#project-four-title").toggle("#project-four");
+    },
+    function() {
+      $("#project-four").animate({ opacity: "1" });
+      $("#project-four-title").toggle("#project-four");
+    }
+  );
+  //project five
+  $("#project-five").hover(
+    function() {
+      $("#project-five").animate({ opacity: "0.35" });
+      $("#project-five-title").toggle("#project-five");
+    },
+    function() {
+      $("#project-five").animate({ opacity: "1" });
+      $("#project-five-title").toggle("#project-five");
+    }
+  );
+  //project six
+  $("#project-six").hover(
+    function() {
+      $("#project-six").animate({ opacity: "0.35" });
+      $("#project-six-title").toggle("#project-six");
+    },
+    function() {
+      $("#project-six").animate({ opacity: "1" });
+      $("#project-six-title").toggle("#project-six");
+    }
+  );
+  //project seven
+  $("#project-seven").hover(
+    function() {
+      $("#project-seven").animate({ opacity: "0.35" });
+      $("#project-seven-title").toggle("#project-seven");
+    },
+    function() {
+      $("#project-seven").animate({ opacity: "1" });
+      $("#project-seven-title").toggle("#project-seven");
+    }
+  );
+  //project eight
+  $("#project-eight").hover(
+    function() {
+      $("#project-eight").animate({ opacity: "0.35" });
+      $("#project-eight-title").toggle("#project-eight");
+    },
+    function() {
+      $("#project-eight").animate({ opacity: "1" });
+      $("#project-eight-title").toggle("#project-eight");
+    }
+  );
+   
 
 })
